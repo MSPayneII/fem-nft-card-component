4366c3cb2b3bbbe9f5e7dd9f4b166d029b7a7cd3
Michael Payne <mpaynei@umich.edu>
2022-01-05 22:06:48 -0500

    updating css and readme file

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import data from "./productData";
 import Project from "./components/Project";
 
 function App() {
-  const [projects, setProject] = useState(data);
+  const [projects, setProject] = useState([]);
+
+  // useEffect is used in preparation for a version 2 build
+  useEffect(() => {
+    setProject(data);
+  }, []);
 
   return (
     <main>
